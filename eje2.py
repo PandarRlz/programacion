4cb00fb7903ae3d4ffaa1341220286cf6e20d1f2
PandarRlz <164398506+PandarRlz@users.noreply.github.com>
2024-04-19 14:12:00 -0400

    import math
    pow.math y round veerlo por repaso

---
+++ eje2.py
@@ -0,0 +1,7 @@
+import math
+ladoa=float(input("Ingrese el lado a: "))
+ladob=float(input("Ingrese el lado b: "))
+ladoc=math.sqrt(math.pow(ladoa,2)+math.pow(ladob,2))
+print("El lado c del triangulo es: ",round(ladoc,2))
+#ladoc=int(input("Ingrese el lado c: "))
+#math.pow(variable y la coma , sera el elevado a cuanto tu desees. ej math.pow(lado2,2 ))
